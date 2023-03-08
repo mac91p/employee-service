8c1ea030ea10740748ca78584341b14239353625
mac91p <mackobws@gmail.com>
2023-03-08 17:29:01 +0100

    added equals() and hashcode()

--- src/main/java/pl/kurs/employeeservice/model/Employee.java
+++ src/main/java/pl/kurs/employeeservice/model/Employee.java
@@ -1,6 +1,8 @@
 package pl.kurs.employeeservice.model;
 
 
+import java.util.Objects;
+
 public class Employee {
 
     private String firstName;
@@ -41,6 +43,19 @@ public class Employee {
 
     public String getCity() {
         return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(pesel, employee.pesel) && Objects.equals(position, employee.position) && Objects.equals(city, employee.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, pesel, salary, position, city);
     }
 
     @Override
